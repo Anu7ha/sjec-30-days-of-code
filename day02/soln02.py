92eecb4c77cc22938f1dbd7da49123737136267b
Anu7ha <anushak1402@gmail.com>
2022-12-21 00:17:18 +0530

    DAY02 DONE

---
+++ day02/soln02.py
@@ -0,0 +1,47 @@
+n=int(input('Enter the number of triangles: '))
+side=[]
+def small(a,b,c):
+    if a<b and a<c:
+        return a
+    elif b<a and b<c:
+        return b
+    else:
+        return c
+
+def medium(a,b,c):
+    if((a<b and a>c) or (a>b and a<c)):
+        return a
+    elif((b<a and b>c) or (b>a and b<c)):
+        return b
+    else:
+        return c
+
+def large(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c  
+for i in range(1,n+1):
+    a=int(input('Enter the 1st side: '))
+    b=int(input('Enter the 2st side: '))
+    c=int(input('Enter the 3rd side: '))
+    print(a,b,c)
+    if i%3==0:
+        side.append(large(a,b,c))
+    elif i%3==1:
+         side.append(small(a,b,c))
+    else:
+         side.append(medium(a,b,c)) 
+print(side)
+    
+
+
+
+
+
+
+
+
+     
