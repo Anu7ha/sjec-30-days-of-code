4425b7505b2c020dca4235cdfcc6c10e402c5a06
Anu7ha <anushak1402@gmail.com>
2022-12-21 22:48:18 +0530

    DAY03 done

---
+++ day03/soln3.py
@@ -0,0 +1,12 @@
+n=int(input('Enter the count: '))
+s=[]
+for i in range(n):
+    s.append(int(input()))
+
+avg=sum(s)/n
+
+print("greater: ")
+for i in s:
+    if (avg<i):
+        print(i)
+        continue
