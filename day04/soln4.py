a6999ac4590d69491c369ee93876749a00bab576
Anu7ha <anushak1402@gmail.com>
2022-12-22 23:00:03 +0530

    DAY04 done

---
+++ day04/soln4.py
@@ -0,0 +1,9 @@
+m= int(input("Enter the rows: "))
+n=int(input("Enter the columns: "))
+
+if(m*n%3==0 and m!=n and ((m%2==0  and n%3==0)or(m%3==0 and n%2==0))):
+    print("yes")
+elif(m==n and m!=3 and m*n%3==0 and (m%3==0 and n%2==0)):
+    print("yes")
+else:
+    print("No")
